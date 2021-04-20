b003c7ac71d09b24e0726dc70874fabf5b2bbb1d
Valeriia <valeriia.air@gmail.com>
2021-04-20 17:08:55 +0300

    настроен препроцессор SASS и его синтаксис SCSS

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -2,25 +2,25 @@ const images = [
   {
     url:
       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    alt: 'White and Black Long Fur Cat'
   },
   {
     url:
       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish'
   },
   {
     url:
       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
+    alt: 'Group of Horses Running'
+  }
+]
 
 const gallery = document.querySelector('#gallery')
 
 const imagesList = images.map(image => {
-  const imageListEl = document.createElement('li');
-  const imageEl = document.createElement('img');
+  const imageListEl = document.createElement('li')
+  const imageEl = document.createElement('img')
 
   imageListEl.insertAdjacentElement('afterbegin', imageEl)
   // imageListEl.appendChild(imageEl)
