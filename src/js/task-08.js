90b1f6dc646257c36b4b704dd5c0c36bce3c0cd3
Valeriia <valeriia.air@gmail.com>
2021-05-04 22:51:54 +0300

    создание коллекции боксов с рандомным цветом

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,39 +1,37 @@
 const refs = {
   renderBtn: document.querySelector('[data-action="render"]'),
   destroyBtn: document.querySelector('[data-action="destroy"]'),
-  boxesContainer: document.querySelector('#boxes'),
+  boxesContainer: document.querySelector('#boxes')
 }
 
-refs.renderBtn.addEventListener('click', getAmount);
-refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.renderBtn.addEventListener('click', getAmount)
+refs.destroyBtn.addEventListener('click', destroyBoxes)
 
-function getAmount() {
-  let amount = document.querySelector('#controls input').value;
-  createBoxes(amount);
+function getAmount () {
+  let amount = document.querySelector('#controls input').value
+  createBoxes(amount)
 }
 
-refs.renderBtn.addEventListener('click', createBoxes(amount));
-
-function createBoxes(amount) {
-  let basicSize = 30;
-  let fragment = document.createDocumentFragment();
+function createBoxes (amount) {
+  let basicSize = 30
+  let fragment = document.createDocumentFragment()
 
   for (let i = 0; i < boxesAmountInput; i++) {
-    let size = basicSize + i * 10;
-    let boxEl = document.createElement('div');
-    boxEl.classList.add('box-element');
-    boxEl.style.backgroundColor = `rgba( ${random()}, ${random()}, ${random()})`;
-    boxEl.style.width = `${size}px`;
-    boxEl.style.height = `${size}px`;
-    fragment.appendChild(boxEl);
+    let size = basicSize + i * 10
+    let boxEl = document.createElement('div')
+    boxEl.classList.add('box-element')
+    boxEl.style.backgroundColor = `rgba( ${random()}, ${random()}, ${random()})`
+    boxEl.style.width = `${size}px`
+    boxEl.style.height = `${size}px`
+    fragment.appendChild(boxEl)
   }
-  refs.boxesContainer.appendChild(fragment);
+  refs.boxesContainer.appendChild(fragment)
 }
 
-function destroyBoxes() {
-  refs.boxesContainer.innerHTML = '';
-};
+function destroyBoxes () {
+  refs.boxesContainer.innerHTML = ''
+}
 
-function random() {
-  return Math.floor(Math.random() * 256);
+function random () {
+  return Math.floor(Math.random() * 256)
 }
